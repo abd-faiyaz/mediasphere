e96c54f221f7d09d838dd9b281d88782bb2e6db3
abd-faiyaz <2005065@cse.buet.ac.bd>
2025-07-14 13:38:53 +0600

    unnecessary files removed

--- add_comment_like_count.sql
+++
@@ -1,7 +0,0 @@
--- Migration to add like_count column to comments table
--- This migration adds comment like count tracking
-
--- Add like_count column to comments table
-alter table comments add column like_count
-
-int;
